30e7e6efff81237bb3682e5c51a6bbfc67305363
Christian Westgaard <cwe@enonic.com>
2018-09-28 11:29:34 +0200

    Upgrade node, modules and gradle

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,8 @@
 import glob from 'glob';
 import path from 'path';
-import webpack from 'webpack';
-import PolyfillsPlugin from 'webpack-polyfills-plugin';
-const MinifyPlugin = require('babel-minify-webpack-plugin');
+//import webpack from 'webpack';
+//import PolyfillsPlugin from 'webpack-polyfills-plugin';
+//const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 const SRC_RESOURCES_DIR = './src/main/resources';
 const SRC_ASSETS_DIR = `${SRC_RESOURCES_DIR}/assets`;
@@ -10,14 +10,14 @@ const DST_RESOURCES_DIR = './build/resources/main';
 const ASSETS_ES6_GLOB = glob.sync(`${SRC_ASSETS_DIR}/**/*.{es,es6,jsx}`);
 const ES6_GLOB = glob.sync(`${SRC_RESOURCES_DIR}/**/*.{es,es6,jsx}`, { absolute: false, ignore: ASSETS_ES6_GLOB });
 
-let entry = ES6_GLOB.reduce((entries, entry) => Object.assign(entries, {[
-    entry.replace(`${SRC_RESOURCES_DIR}/`, '').replace(/\.(es6?|jsx)/, '.js')
-]: [
+const entry = ES6_GLOB.reduce((entries, e) => Object.assign(entries, {
+  [e.replace(`${SRC_RESOURCES_DIR}/`, '').replace(/\.(es6?|jsx)/, '.js')]: [
   // TypeError: Cannot read property "__core-js_shared__" from undefined (com.enonic.xp.resource.ResourceProblemException)
   // var store = global[SHARED] || (global[SHARED] = {});
   //'core-js/fn/map',
-  entry
-]}), {});
+    e
+  ]
+}), {});
 //entry['assets/js/scripts.js'] = `${SRC_ASSETS_DIR}/js/scripts.es6`;
 
 
@@ -29,7 +29,7 @@ const WEBPACK_CONFIG = {
   devtool: false, // Don't waste time generating source-maps
   output: {
     path: path.join(__dirname, DST_RESOURCES_DIR), // Must be absolute
-    filename: "[name]",
+    filename: '[name]',
     libraryTarget: 'commonjs'
   },
   mode: 'production',
@@ -44,31 +44,23 @@ const WEBPACK_CONFIG = {
           compact: false,
           minified: false,
           plugins: [
-            'array-includes',
-            'optimize-starts-with',
-            'transform-object-rest-spread'
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-transform-object-assign',
+            'array-includes'
           ],
           presets: [
-            [
-              'env', {
-                modules: false, // Let minifyer handle modules...
-                // modules: 'commonjs', // commonjs is the default
-                targets: {
-                  // browserlist combined by OR clause
-                  // https://github.com/ai/browserslist
-                  browsers: ['last 2 versions', 'not ie <= 10']
-                },
-                useBuiltIns: 'usage', // won't work before babel-preset-env-2.x
-                debug: true
+            /*[
+              '@babel/preset-env', {
+                useBuiltIns: false // false means polyfill not required runtime
               }
-            ],
-            'react' // Currently doesn't accept options.
+            ],*/
+            '@babel/preset-react'
           ]
         }
       }
     ] // loaders
-  }, // module
-  /*node: {
+  }/*, // module
+  node: {
     console: false,
     global: true, // Needed by babel-polyfill, but didn't help :(
     process: true,
@@ -77,27 +69,27 @@ const WEBPACK_CONFIG = {
     Buffer: true,
     setImmediate: true
   },*/
-  plugins: [
-    /*{
-      'apply': function(compiler) {
-        compiler.parser.plugin('expression global', function() {
+  /*plugins: [
+    {
+      apply: compiler => {
+        compiler.parser.plugin('expression global', () => {
           this.state.module.addVariable('global', "(function() { return this; }()) || Function('return this')()");
           return true;
         });
       }
-    },*/
-    /*new webpack.ProvidePlugin({
+    },
+    new webpack.ProvidePlugin({
       //Map: 'core-js/fn/map'
       //Map: 'imports-loader?this=>global!exports-loader?global.Map!core-js/fn/map'
       Map: 'es6-map' // Cannot read property "call" from undefined
       //Map: 'es6-map/polyfill'
-    })*/
-    //new PolyfillsPlugin([
-      //'Map'
-   //])
-    /*new MinifyPlugin({ //minifyOpts
+    }),
+    new PolyfillsPlugin([
+      'Map'
+    ]),
+    new MinifyPlugin({ //minifyOpts
     }, { //pluginOpts
-    }) // MinifyPlugin*/
-  ] // plugins
+    }) // MinifyPlugin
+  ]*/ // plugins
 };
 export default WEBPACK_CONFIG;
